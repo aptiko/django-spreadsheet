dc5a3e641714f991982b096d0487ba893758dbe2
Antonis Christofides <antonis@antonischristofides.com>
2024-12-18 13:34:41 +0200

    Use more modern black

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,9 +4,9 @@ import sys
 import django
 from django.core import management
 
-os.environ[
-    "DJANGO_SETTINGS_MODULE"
-] = "tests.testdjangoproject.testdjangoproject.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = (
+    "tests.testdjangoproject.testdjangoproject.settings"
+)
 sys.path.append("tests/testdjangoproject")
 django.setup()
 management.call_command("migrate")
